28366b0cc68050637992b92cd14c4739f88b7e33
aarora16 <a.99.anshul@gmail.com>
2021-04-05 16:22:51 -0500

    added enums fro data-tests

---
+++ src/typings/data-test-enums.ts
@@ -0,0 +1,22 @@
+// define enum here
+export enum dataTest {
+  '[data-test="left-summary-table"]' = 1,
+  '[data-test="right-summary-table"]',
+  '[data-test="PREV_CLOSE-value"]',
+  '[data-test="OPEN-value"]',
+  '[data-test="BID-value"]',
+  '[data-test="ASK-value"]',
+  '[data-test="DAYS_RANGE-value"]',
+  '[data-test="FIFTY_TWO_WK_RANGE-value"]',
+  '[data-test="TD_VOLUME-value"]',
+  '[data-test="AVERAGE_VOLUME_3MONTH-value"]',
+  '[data-test="MARKET_CAP-value"]',
+  '[data-test="BETA_5Y-value"]',
+  '[data-test="PE_RATIO-value"]',
+  '[data-test="EPS_RATIO-value"]',
+  '[data-test="EARNINGS_DATE-value"]',
+  '[data-test="DIVIDEND_AND_YIELD-value"]',
+  '[data-test="EX_DIVIDEND_DATE-value"]',
+  '[data-test="ONE_YEAR_TARGET_PRICE-value"]',
+}
+
